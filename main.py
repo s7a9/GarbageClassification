08b40cc01e40f3c4d7c943e1e49b6ea2c8c335e2
leoliu <leoliu@ebay.com>
2020-08-21 20:57:30 +0800

    修复loss计算错误

--- main.py
+++ main.py
@@ -33,7 +33,10 @@ BATCH_SIZE = args.bs
 
 logging.info('begin to load data...')
 train_loader, test_loader = get_loaders(0.8, BATCH_SIZE, args.aug)
-
+train_len = len(train_loader.dataset)
+test_len = len(test_loader.dataset)
+logging.info(f'size of training set: {train_len}')
+logging.info(f'size of testing set: {test_len}')
 
 device = torch.device('cuda')
 # model = ResNet18().to(device)
@@ -41,37 +44,46 @@ model = model_dict[args.model]
 num_in = model.fc.in_features
 model.fc = nn.Linear(num_in, 40)
 model = model.to(device)
-print(model, flush= True) 
+print(model, flush= True)
 
 use_scheduler = args.scheduler != 'None'
 if use_scheduler:
     optimizer, scheduler = get_optimizer(model, args.lr, args.scheduler, args.optim)
 else:
     optimizer = get_optimizer(model, args.lr, args.scheduler, args.optim)
+# optimizer = optim.SGD(model.parameters(), lr=1e-4)  #TODO
 criteon = nn.CrossEntropyLoss()
 
 logging.info('begin to train...')
 for epoch in range(args.epoch):
+    train_loss = 0
+    test_loss = 0
+
+    model.train()
     for x, label in train_loader:
         x, label = x.to(device), label.to(device)
         logits = model(x)
         loss = criteon(logits, label)
+        train_loss += loss.item()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         if use_scheduler:
             scheduler.step()
-    logging.info(f'[{epoch}] loss: {loss.item()}')
+
+    model.eval()
     with torch.no_grad():
         total_corret = 0
         total_num = 0
         for x, label in test_loader:
             x, label = x.to(device), label.to(device)
             logits = model(x)
+            loss = criteon(logits, label)
+            test_loss += loss.item()
             pred = logits.argmax(dim=1)
             correct = torch.eq(pred, label).float().sum().item()
             total_corret += correct
             total_num += x.size(0)
         acc = total_corret / total_num
-        print('Test accuracy:', acc, flush= True)
-    
+
+    logging.info(f'[{epoch}] train_loss: {train_loss / train_len}, test_loss: {test_loss / test_len}, test_accuracy: {acc}')
